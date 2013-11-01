6f39e50c813f0d0760e96b982a7e9940026ee505
Evan Noon <epnoon@umich.edu>
2013-11-01 15:36:26 -0400

    Not working, but I'm working on it

--- src/edu/umich/yourcast/FieldActivity.java
+++ src/edu/umich/yourcast/FieldActivity.java
@@ -1,5 +1,8 @@
 package edu.umich.yourcast;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -7,58 +10,64 @@ import org.json.JSONObject;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+@SuppressWarnings("unused")
 public class FieldActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_field);
+		Intent intent = getIntent();
+		String json = intent.getStringExtra(MainActivity.MATCH_INFO);
+		JSONObject match_info;
+		String home = null, away = null, time = null;
+		try {
+			match_info = new JSONObject(json);
+			home = (String) match_info.getString("home team");
+			away = (String) match_info.getString("away team");
+			time = (String) match_info.getString("time");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_field);
-	Intent intent = getIntent();
-	String json = intent.getStringExtra(MainActivity.MATCH_INFO);
-	JSONObject match_info;
-	String home = null, away = null, time = null;
-	try {
-	    match_info = new JSONObject(json);
-	    home = (String) match_info.getString("home team");
-	    away = (String) match_info.getString("away team");
-	    time = (String) match_info.getString("time");
-	} catch (JSONException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
+		String output = home + " vs. " + away;
+		TextView t = (TextView) findViewById(R.id.gametitle);
+		t.setText(output);
+
+		// ImageView field = (ImageView) findViewById(R.id.fieldimage);
+
+		Log.d("MYMY", output);
 	}
 
-	String output = home + " vs. " + away;
-	TextView t = (TextView) findViewById(R.id.gametitle);
-	t.setText(output);
-
-	Log.d("MYMY", output);
-    }
-
-    // TODO Dummy method to put in current game Info,
-    // should be changed later to listen to event and update game info
-    public String getGameInfo() {
-	JSONObject object = new JSONObject();
-	try {
-	    object.put("game score", "1-1");
-	    object.put("game time", "90:00");
-	} catch (JSONException e) {
-	    e.printStackTrace();
+	// TODO Dummy method to put in current game Info,
+	// should be changed later to listen to event and update game info
+	public String getGameInfo() {
+		JSONObject object = new JSONObject();
+		try {
+			object.put("game score", "1-1");
+			object.put("game time", "90:00");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		Log.d("MYMY", "getGameInfo");
+		return object.toString();
 	}
-	Log.d("MYMY", "getGameInfo");
-	return object.toString();
-    }
 
-    public void infoButtonClick(View view) {
-	GameInfoDialog dialog = GameInfoDialog.create(getGameInfo());
-	dialog.show(getFragmentManager(), "GameInfoDialog");
-    }
+	public void infoButtonClick(View view) {
+		GameInfoDialog dialog = GameInfoDialog.create(getGameInfo());
+		dialog.show(getFragmentManager(), "GameInfoDialog");
+	}
 
 }
